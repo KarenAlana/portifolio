a3e45e07e7341b6bc506b3047ad4a592098358eb
KarenAlana <104793049+KarenAlana@users.noreply.github.com>
2022-09-09 18:39:42 -0300

    Add files via upload

---
+++ js/js-02.js
@@ -0,0 +1,231 @@
+var btn1 = document.querySelector(".action-btn-a");
+var container1 = document.querySelector(".container-a");
+var close1 = document.querySelector(".close-a");
+
+btn1.addEventListener("click", function () {
+  if (container1.style.display === "block") {
+    container1.style.display = "none";
+  } else {
+    container1.style.display = "block";
+  }
+})
+
+close1.addEventListener("click", function () {
+  if (container1.style.display === "none") {
+    container1.style.display = "block";
+  } else {
+    container1.style.display = "none";
+  }
+});
+
+
+var btn2 = document.querySelector(".action-btn-b");
+var container2 = document.querySelector(".container-b");
+var close2 = document.querySelector(".close-b");
+
+btn2.addEventListener("click", function () {
+  if (container2.style.display === "block") {
+    container2.style.display = "none";
+  } else {
+    container2.style.display = "block";
+  }
+});
+close2.addEventListener("click", function () {
+  if (container2.style.display === "none") {
+    container2.style.display = "block";
+  } else {
+    container2.style.display = "none";
+  }
+});
+
+
+var btn3 = document.querySelector(".action-btn-c");
+var container3 = document.querySelector(".container-c");
+var close3 = document.querySelector(".close-c");
+
+btn3.addEventListener("click", function () {
+  if (container3.style.display === "block") {
+    container3.style.display = "none";
+  } else {
+    container3.style.display = "block";
+  }
+});
+
+close3.addEventListener("click", function () {
+  if (container3.style.display === "none") {
+    container3.style.display = "block";
+  } else {
+    container3.style.display = "none";
+  }
+});
+
+
+
+var btn4 = document.querySelector(".action-btn-d");
+var container4 = document.querySelector(".container-d");
+var close4 = document.querySelector(".close-d");
+
+btn4.addEventListener("click", function () {
+  if (container4.style.display === "block") {
+    container4.style.display = "none";
+  } else {
+    container4.style.display = "block";
+  }
+});
+
+close4.addEventListener("click", function () {
+  if (container4.style.display === "none") {
+    container4.style.display = "block";
+  } else {
+    container4.style.display = "none";
+  }
+});
+
+
+var btn5 = document.querySelector(".action-btn-e");
+var container5 = document.querySelector(".container-e");
+var close5 = document.querySelector(".close-e");
+
+btn5.addEventListener("click", function () {
+  if (container5.style.display === "block") {
+    container5.style.display = "none";
+  } else {
+    container5.style.display = "block";
+  }
+});
+
+close5.addEventListener("click", function () {
+  if (container5.style.display === "none") {
+    container5.style.display = "block";
+  } else {
+    container5.style.display = "none";
+  }
+});
+
+
+var btn6 = document.querySelector(".action-btn-f");
+var container6 = document.querySelector(".container-f");
+var close6 = document.querySelector(".close-f");
+
+btn6.addEventListener("click", function () {
+  if (container6.style.display === "block") {
+    container6.style.display = "none";
+  } else {
+    container6.style.display = "block";
+  }
+});
+
+close6.addEventListener("click", function () {
+  if (container6.style.display === "none") {
+    container6.style.display = "block";
+  } else {
+    container6.style.display = "none";
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var itens = document.getElementsByClassName("songs-container"); //pega todos elementos da class uso não obrigatorio de . 
+console.log(itens);
+
+var itensonly = document.querySelectorAll(".songs-container p"); //pega todos os elementos dentro da class ou id uso obrigatorio de . e #
+console.log(itensonly);
+
+var itensjustonly = document.querySelector(".songs-container")//pega apenas um elemento
+console.log(itensjustonly);
+
+var itensjustonlys = document.querySelectorAll(".songs-container p")[0]//tem que colocar o 0 pq ele me da uma node list
+console.log(itensjustonly);*/
+
+/*var btn = document.querySelector('.action-btn-b')
+var container = document.querySelector('.container-b')
+
+		btn.addEventListener('click', function(){
+			
+			if(container.style.display === 'block'){
+				container.style.display = 'none';
+			} else {
+				container.style.display = 'block';
+			}
+
+});*/
+
+/*var btn1 = document.querySelector(".action-btn-1");
+var container1 = document.querySelector(".container-1");
+var bt2 = document.querySelector(".action-btn-2");
+var container2 = document.querySelector(".container2");
+var btn3 = document.querySelector(".action-btn-3");
+var container3 = document.querySelector(".container-3");
+var btn4 = document.querySelector(".action-btn-4");
+var container4 = document.querySelector(".container-4");
+var btn5 = document.querySelector(".action-btn-5");
+var container5 = document.querySelector(".container-5");
+var btn6 = document.querySelector(".action-btn-6");
+var container6 = document.querySelector(".container-6");
+
+
+btn1.addEventListener("click", function () {
+	 if (container1.style.display === "block") {
+     container1.style.display = "none";
+   }else{
+     container1.style.display = "block";
+   }
+ 
+});
+btn2.addEventListener("click", function () {
+	 if (container.style.display === "block") {
+     container.style.display = "none";
+   }else{
+     container.style.display = "block";
+   }
+ 
+});
+btn3.addEventListener("click", function () {
+	 if (container.style.display === "block") {
+     container.style.display = "none";
+   }else{
+     container.style.display = "block";
+   }
+ 
+});
+btn4.addEventListener("click", function () {
+	 if (container.style.display === "block") {
+     container.style.display = "none";
+   }else{
+     container.style.display = "block";
+   }
+ 
+});
+btn5.addEventListener("click", function () {
+	 if (container.style.display === "block") {
+     container.style.display = "none";
+   }else{
+     container.style.display = "block";
+   }
+ 
+});
+btn6.addEventListener("click", function () {
+	 if (container.style.display === "block") {
+     container.style.display = "none";
+   }else{
+     container.style.display = "block";
+   }
+ 
+});
+*/
